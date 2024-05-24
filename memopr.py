f640f75e82d9e74db6be7e4af05e666648d28c9a
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-24 10:44:33 +0530

    operator

---
+++ memopr.py
@@ -0,0 +1,13 @@
+string1="mukesh"
+print("m" in string1)
+print("k" in string1)
+print("s" in string1)
+print("h" in string1)
+print("b" in string1)
+print("g" not in string1)
+print("l" not in string1)
+m=[20,23,56,86,96,94]
+print(90 not in m)
+print(96 in m)
+print(20 in m)
+print(60 in m)

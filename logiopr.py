09ece484d5bfd7676a31ecd0f544a67794c079c5
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-24 10:43:36 +0530

    operator

---
+++ logiopr.py
@@ -0,0 +1,14 @@
+x=50
+y=60
+print(x==50 and x<y)
+print(y==60 and y>x)
+print(x==y and x<y)
+print(x==50 or x<y)
+print(y==60 or x>y)
+print(x==y or x<y)
+print(x!=50)
+print(x!=30)
+print(not x!=50)
+print(not x!=30)
+print(y==60)
+print(not y==60)

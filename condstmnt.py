386485b1370f46ffb0864760192b25dc882b0146
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-27 13:50:42 +0530

    conditional statement

---
+++ condstmnt.py
@@ -0,0 +1,12 @@
+a=int(input("enter the value:- "))
+if a%2==0:
+    print(a,"even number")
+b=int(input("enter the value:- "))
+if b%2==1:
+    print(b,"odd number")
+c=int(input("enter the value:- "))
+if c%2==1:
+    print(c,"odd number")
+else:
+    print(c,"even number")
+    

f2969923944efa610971645c2df74c4fdacddffa
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-27 13:49:09 +0530

    Add files via upload

---
+++ userin.type.c.py
@@ -0,0 +1,17 @@
+a=input("enter the value:- ")
+print(a)
+b=input("enter the value1:- ")
+c=input("enter the value2:- ")
+print(b+c)
+a=int(input("enter the value1:- "))
+b=int(input("enter the value2:- "))
+print(a+b)
+print(a*b)
+a=float(input("enter the value1:- "))
+b=float(input("enter the value2:- "))
+print(a+b)
+a=eval(input("enter the value1:- "))
+b=eval(input("enter the value2:- "))
+print(a+b)
+
+

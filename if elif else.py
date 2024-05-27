4b563cd77cb469da572559a9381cbd92e4143eb2
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-27 13:51:26 +0530

    if elif else statement

---
+++ if elif else.py
@@ -0,0 +1,9 @@
+per=int(input("enter the value"))
+if per>=60:
+    print("first division")
+elif per>=48:
+    print("second division")
+elif per>=36:
+    print("third division")
+else:
+    print("fail")

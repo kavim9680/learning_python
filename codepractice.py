62a3e90954975db2fde1aee5dd8f26315bc638ab
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-28 14:13:59 +0530

    simple code

---
+++ codepractice.py
@@ -0,0 +1,14 @@
+a=int(input("enter the value1:- "))
+b=int(input("enter the value2:- "))
+opr=input("enter the value3:-")
+if opr=="+":
+    print(a+b)
+elif opr=="-":
+    print(a-b)
+elif opr=="*":
+    print(a*b)
+elif opr=="/":
+    print(a/b)
+else:
+    print("invalid operation...")
+

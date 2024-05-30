0ad7897abf5619bfdbd30fb9f496954676e50648
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-30 13:19:50 +0530

    string function

---
+++ string function.py
@@ -0,0 +1,109 @@
+##lower function
+
+a="HELLO MUKESH YADAV"
+print(a.lower())
+b=a.lower()
+print(b)
+
+
+##upper function
+
+a="hello mukesh yadav"
+print(a.upper())
+b=a.upper()
+print(b)
+
+
+##title function
+
+a="my name is mukesh yadav. my brother name is brijesh yadav."
+print(a.title())
+b=a.title()
+print(b)
+
+
+##capital function
+
+a="my name is Arush yadav"
+print(a.capitalize())
+b=a.capitalize()
+print(b)
+
+##find function
+
+a="hello mukesh yadav"
+print(a.find("e"))
+b=a.find("e")
+print(b)
+print(a.find("e",2))
+
+##index function
+
+a="education is the most powerful weapon"
+print(a.index("a"))
+b=a.index("a")
+print(b)
+print(a.index("a",5))
+
+##isalpha function
+
+a="honesty is the best policy"
+print(a.isalpha())
+b=a.isalpha()
+print(b)
+
+str="jaihanuman"
+print(str.isalpha())
+str1="mukesh yadav"
+print(str1.isalpha())
+
+##isdigit function
+
+str2="jai maa saraswati"
+print(str2.isdigit())
+s2=str2.isdigit()
+print(s2)
+
+str3="9621833972"
+print(str3.isdigit())
+s3=str3.isdigit()
+print(s3)
+
+##isalnumfunction
+
+str4="india9621"
+print(str4.isalnum())
+s4=str4.isalnum()
+print(s4)
+
+##chrfunction
+
+a1=65
+print(chr(a1))
+a2=66
+print(chr(a2))
+a3=67
+print(chr(a3))
+
+##ord function
+
+a4="A"
+print(ord(a4))
+a5="a"
+print(ord(a5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

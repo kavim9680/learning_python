6e5742ea982c75c5f384c59ce694edccd3bb6e57
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-30 13:25:04 +0530

    slicing of a list

---
+++ slicing of a list.py
@@ -0,0 +1,9 @@
+l1=[20,30,40,50,60,70,"mukesh",[1,2,3]]
+print(l1[2],l1[6],l1[7])
+print(l1[0:3])
+print(l1[1: :2])
+print(l1[-1: :-1])
+print(l1[-1: :-2])
+print(len(l1))
+print(l1[0:8:2])
+print(l1[1:6:2])

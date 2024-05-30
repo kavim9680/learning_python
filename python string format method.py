3d4363f6335027ca9f85312de54970f9a37a61f4
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-30 13:18:55 +0530

    string format method in python

---
+++ python string format method.py
@@ -0,0 +1,30 @@
+##first method
+
+str1="hello {} yadav welcome to {} ".format("mukesh","noida");
+print(str1)
+
+##second method
+
+str2="my {0} is mukesh yadav my pin code no. is {1} ".format("name",222136);
+print(str2)
+
+##third method
+
+str3="my country code is {a} and my pin no {b} ".format(a=9621,b=222136)
+print(str3)
+
+str4="my school code {a:12} and my village code{b:14} ".format(a=9621,b=8052)
+print(str4)
+
+str5="my college code {a:^12} and my village code {b:^14} ".format(a=9621,b=8052)
+print(str5)
+
+str6="my college code {a:>10}".format(a=20)
+print(str6)
+
+str7="my pin code {a:<10}".format(a=60)
+print(str7)
+
+
+
+

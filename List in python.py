93ae2b5e4f2b69b502d3e3dc552d6edf693e2ac9
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-30 13:23:43 +0530

    list in a python

---
+++ List in python.py
@@ -0,0 +1,14 @@
+##create a list and check list
+
+l1=[1,2,3,4,5]
+print(l1,type(l1))
+l2=[2,4,"hello"]
+print(l2,type(l2))
+l3=[3,4,"hello",[1,2]]
+print(l3,type(l3))
+
+##list is a mutable or changeable
+##list ia always declared square bracket[]
+##in a list contain any value, character and string with sepreted to commos
+
+

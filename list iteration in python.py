d39843f49de5f656608024d5d341f2e9edff76bc
kavim9680 <mukeshyadav962197@gmail.com>
2024-05-31 11:45:37 +0530

    list iteration

---
+++ list iteration in python.py
@@ -0,0 +1,20 @@
+##first method iteration useing for loop:
+
+l=[10,20,30,40,50,60,70,80,90,100,"hello","mukesh"]
+t=len(l)
+for n in range(t):
+    print(l[n])
+
+##second method iteration of list:
+
+l1=[1,2,3,4,5,6,7,8,9,"hello","mukesh"]
+for a in l:
+    print(a)
+
+##reverse iteration using for loop:
+
+l2=[2,4,6,8,10,12,14,16,18,20]
+t=len(l2)
+for n in range(t-1,-1,-1):
+    print(l2[n])
+

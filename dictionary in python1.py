8498fcfbe1d91594921abd6b3bb2519bd914c671
kavim9680 <mukeshyadav962197@gmail.com>
2024-06-04 13:35:05 +0530

    dictionary

---
+++ dictionary in python1.py
@@ -0,0 +1,18 @@
+##dictionary:
+#dictionary is a unordered data type:
+#dictionary works the key an value pair:
+#dictionary define the carlee bracket{}:
+#dictionary is mutable or changeable:
+
+d={
+    'name': "mukesh yadav",
+    'class': 12,
+    'age': 21,
+    'college name': "kutir inter college chakkey"
+}
+print(d,type(d))
+print(d['age'])
+print(d['name'])
+print(d['class'])
+print(d['college name'])
+

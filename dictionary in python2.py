4ac4443576e44adb236d9e279f1885360efeeef4
kavim9680 <mukeshyadav962197@gmail.com>
2024-06-04 13:36:29 +0530

    dictionary function

---
+++ dictionary in python2.py
@@ -0,0 +1,13 @@
+#print all element of a dictionary by using loop: this is the first  method:
+d={
+    'name': 'mukesh yadav',
+    'father name': 'ramashankar yadav',
+    'mother name': 'kamala devi',
+    'brother name': 'brijesh yadav',
+    'my cllege name': 'ktir inter college chakkey',
+    'age': 21,
+    'pin code': 222136
+}
+for n in d:
+    print(n)
+    print(d[n])
